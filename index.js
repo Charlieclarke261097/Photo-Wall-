ad8295c3561ba0b0bbbc96b62387a763d07e0f49
Charlieclarke261097 <32364954+Charlieclarke261097@users.noreply.github.com>
2018-05-01 13:59:42 +0100

    2
    2

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-//Backendless table API key reference
-Backendless.initApp("3F4DF6B3-BD68-2097-FFF1-1E29A6C5EC00","89A5E954-BFC0-5152-FF19-99AB46D7CB00");
 
-//Check that the page has loaded
-$(document).on("pageshow","#page2", onPageShow);
-function onPageShow() {
-    
 
-console.log("page shown");
-    
-}
+
+var APPID = "3F4DF6B3-BD68-2097-FFF1-1E29A6C5EC00";
+var SECRETKEY = "89A5E954-BFC0-5152-FF19-99AB46D7CB00";
+
+Backendless.initApp( APPID, SECRETKEY);
+
 
 var destinationType; //used sets what should be returned (image date OR file path to image for example)
 
@@ -19,71 +16,77 @@ function onDeviceReady() {
 }
 
 function capturePhoto() {
-    alert("capturePhoto");
-	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-	destinationType: destinationType.FILE_URI });
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+    destinationType: window.Camera.DestinationType.FILE_URI
+});
 }
-
+	
 function onPhotoDataSuccess(imageURI) {
-     alert("onPhoto" + imageURI);
+	//var image = document.getElementById('image');
+	//image.style.display = 'block';
+	//image.src = imageURI;
     
-    
-	var image = document.getElementById('myImage');
-	image.style.display = 'block';
-	image.src = imageURI ;
-   localStorage.setItem("imageURI",imageURI);
-    	
-	alert("Photo saved" + imageURI);
-    
-    onAddPhoto(imageURI);
+    //save file to backendless
+
+    window.resolveLocalFileSystemURL(imageURI,gotFileEntry,onFail);
+     
 }
 
 
-function onFail(message) {
-      alert('Failed because: ' + message);
+function gotFileEntry(fileEntry){
+    console.log("gotFileEntry " + fileEntry);
+    fileEntry.file(gotFile,onFail);
 }
-
-function onAddPhoto(imageURI) {
-alert("AddPhoto");  
     
-var imageData = (localStorage.imageURI);
-
-alert("Variable saved" + imageData); 
- 
- //Array of variables
-
-var information = {
-	fileLocation : imageData,
+function gotFile(fileObject){
+    console.log("gotFile " + fileObject);
+    
+       console.log("File Uploaded " + fileObject.fullPath);
+    
+    console.log("File Uploaded " + fileObject.type);
+    
+	 console.log("File Uploaded " + fileObject.size);
 	
-};
+    console.log("File Uploaded " + fileObject.name);
     
-alert("Array created" + imageData); 
     
-//save array to backendless
- Backendless.Data.of("Information").save(information).then(saved).catch(error);
-	
+    
+    var reader = new FileReader();
+
+        reader.onloadend = function() {
+            console.log("Successful file read: " + this.result);
+            console.log("File Uploaded " + fileObject.fullPath);
+            
+            var byteArray = new Blob([new Uint8Array(this.result)], { type: "image/jpg" });
+            
+            Backendless.Files.saveFile( "testfolder", fileObject.name, byteArray, true )
+ .then( function( savedFileURL ) {
+    console.log( "file has been saved - " + savedFileURL );
+Backendless.Data.of("Information").save(savedFileURL).then(saved).catch(error);
+  }
+      )
+ .catch( function( error ) {
+    console.log( "error - " + error.message );
+  }); 
+        };
+
+        reader.readAsArrayBuffer(fileObject);
+    
+    
+    
+    
+
+}
+    
+
+
+
 function saved(savedImage) {
 console.log( "new image has been saved" + savedImage);  
 }
-    
-    
-function error(err){
-     alert(err); 
- }  
 
-alert("URI saved"); 
 
-    
- Backendless.Data.of("Information").find().then(processResults).catch(error);
-    
- alert("Data processed");   
-    
-    
-    
-  function error(err){
-     alert(err); 
- }  
-    
+
 function processResults(Information) {
 alert("processResults"); 
 //Empty the content 
@@ -97,25 +100,21 @@ for(var i = 0; i < Information.length;i++){
 	
     
 alert( Information[i].fileLocation)
- alert("Processed"); 
+ 
+alert("Processed"); 
 
 	
 	
 
 
 
-
-}   
-    
-    
-
-
-    
 
 }
 
 
-
+function onFail(message) {
+      console.log('Failed because: ' + message);
+}
 
  
  
